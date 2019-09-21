0470d45cb437f16e0fde12f7633fde987ea14ccc
Pallop Chaoputhipuchong <pallop.chao@gmail.com>
2019-09-21 18:01:12 +0700

    Add repository link

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,64 @@
-import {Link as GatsbyLink} from 'gatsby'
+import {Link as GatsbyLink, graphql, useStaticQuery} from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 import styled from '@emotion/styled'
+import Img from 'gatsby-image'
+
+const GitHubLinkWrapper = styled.a`
+  transition: opacity 0.3s ease;
+  opacity: 0.2;
+  &:hover {
+    opacity: 0.8;
+  }
+`
 
 const Link = styled(GatsbyLink)`
   color: #111;
   text-decoration: none;
 `
 const HeaderTitle = styled.h1`
+  width: 100%;
   text-align: center;
-  margin: 0;
-  padding: 1rem 0;
+  margin-left: -36px;
 `
 const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  padding: 1rem;
   margin-bottom: 1.45rem;
 `
 
+const GitHubLink = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: {eq: "github-mark.png"}) {
+        childImageSharp {
+          # Specify the image processing specifications right in the query.
+          # Makes it trivial to update as your page's design changes.
+          fixed(width: 36, height: 36) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <GitHubLinkWrapper
+      href="https://github.com/hamcompe/kohitory"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Img fixed={data.file.childImageSharp.fixed} />
+    </GitHubLinkWrapper>
+  )
+}
+
 const Header = ({siteTitle}) => (
   <HeaderContainer>
-    <div>
-      <HeaderTitle>
-        <Link to="/">{siteTitle}</Link>
-      </HeaderTitle>
-    </div>
+    <GitHubLink />
+    <HeaderTitle>
+      <Link to="/">{siteTitle}</Link>
+    </HeaderTitle>
   </HeaderContainer>
 )
 
