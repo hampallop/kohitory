8e9a715fd9ab006a3da8ec467395257c04ba087f
Pallop Chaoputhipuchong <pallop.chao@gmail.com>
2020-01-11 15:34:16 +0700

    Add ratio adjustment

--- src/page-specific/home/home.tsx
+++ src/page-specific/home/home.tsx
@@ -7,7 +7,19 @@ import MoodSelectionSection from './mood-selection-section'
 import ScaleSection from './scale-section'
 import SummarizeSection from './summarize-section'
 
-enum Proportion {
+const InputWrapper = styled.div`
+  display: inline;
+  margin-left: 1rem;
+
+  :first-of-type {
+    margin: 0;
+  }
+  input[type='radio'] {
+    margin-right: 0.25rem;
+  }
+`
+
+enum Ratio {
   fifteen = 15,
   tweleve = 12,
 }
@@ -17,7 +29,7 @@ enum CoffeeBeanSelection {
   twenty = 20,
 }
 
-const proportions = [Proportion.fifteen, Proportion.tweleve]
+const ratioes = [Ratio.fifteen, Ratio.tweleve]
 
 const possibleMoods = [
   {label: 'just a sip', value: CoffeeBeanSelection.ten},
@@ -27,21 +39,35 @@ const possibleMoods = [
 
 const IndexPage = () => {
   const [beansAmount, setBeansAmount] = React.useState(null)
-  const [selectedProportion, setProportion] = React.useState(Proportion.fifteen)
+  const [selectedRatio, setRatio] = React.useState(Ratio.fifteen)
 
   return (
     <>
       <Layout>
         <SEO title="Home" />
+        <h2>Ratio:</h2>
+        {ratioes.map(ratio => (
+          <InputWrapper>
+            <input
+              type="radio"
+              id={`ratio-${ratio}`}
+              name="ratio"
+              value={ratio}
+              checked={`${ratio}` === `${selectedRatio}`}
+              onChange={e => setRatio(Number(e.target.value))}
+            />
+            <label htmlFor={`ratio-${ratio}`}>1:{ratio}</label>
+          </InputWrapper>
+        ))}
         <MoodSelectionSection
           selected={beansAmount}
           onSelect={setBeansAmount}
           possibleMoods={possibleMoods}
         />
-        <ScaleSection portion={beansAmount * selectedProportion} />
+        <ScaleSection portion={beansAmount * selectedRatio} />
       </Layout>
       {beansAmount && (
-        <SummarizeSection beanAmount={beansAmount * selectedProportion} />
+        <SummarizeSection beanAmount={beansAmount * selectedRatio} />
       )}
     </>
   )
